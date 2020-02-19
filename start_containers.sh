ff598173b61526ca659d5b756573ec87366a64a3
advaitpathak <41463217+advaitpathak@users.noreply.github.com>
2020-02-19 14:01:28 +0530

    Add files via upload

---
+++ start_containers.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Start nginx container
+docker-compose up -d
---
+++ start_nginx.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Clone the GIT Repository
+git clone
+
+# Start nginx
+sudo cd web_server
+bash start_containers.sh
+
+
